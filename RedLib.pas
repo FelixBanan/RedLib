cac995041e112b31874e46264b02d37dab2a5bd1
FelixBanan <FelixASD@ya.ru>
2019-04-17 16:22:25 +0300

    Update RedLib.pas

--- RedLib.pas
+++ RedLib.pas
@@ -1,16 +1,28 @@
 unit RedLib;
 
 (*
- Version 1.0.0 Alpha
+ Version 1.0.1 Alpha
  github.com/FelixBanan/Pascal/Lib/RedLib
 *)
 interface
 
 uses crt;
-
+  
+///Это тот же write только возможно выбрать цвет(только цифрой)
+///Вместо запятой, ставится + для объединения переменных
+///Пример: wrc(a+b+'test'+1, 10);
 procedure wrc(text: string; color: integer := 15);
+///Это тот же writeln только возможно выбрать цвет(только цифрой)
+///Вместо запятой, ставится + для объединения переменных
+///Пример: wrcl(a+b+'test'+1, 10);
 procedure wrcl(text: string; color: integer := 15);
+///Это тот же write только укороченный
+///Вместо запятой, ставится + для объединения переменных
+///Пример: wr(a+b+'test'+1);
 procedure wr(text: string);
+///Это тот же writeln только укороченный
+///Вместо запятой, ставится + для объединения переменных
+///Пример: wrl(a+b+'test'+1);
 procedure wrl(text: string);
 implementation
 
@@ -42,6 +54,21 @@ begin
   textcolor(white);
 end;
 
-begin
+var
+  QRED: integer;
 
+begin
+  wrcl('*************************************', 10);
+  wrcl('Red Library version 1.0.1', 10);
+  wrcl('Special Lib for simplify coding', 10);
+  wrcl('by FelixBanan', 10);
+  wrcl('*************************************', 10);
+  wrc('Loading Red Library', 10);
+  while QRED <> 3 do
+  begin
+    QRED := QRED + 1;
+    wrc('.', 10);
+    delay(1500);
+  end;
+  clrscr;
 end. 
