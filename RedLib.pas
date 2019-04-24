9cd5391953de9fcc89dbf8aef2f76de5500b474d
FelixBanan <FelixASD@ya.ru>
2019-04-24 23:02:07 +0300

    read, readln test

--- RedLib.pas
+++ RedLib.pas
@@ -2,7 +2,7 @@ unit RedLib;
 
 (*
  test branch
- Version 1.1.0 Alpha
+ Version 1.3.0 Alpha
  github.com/FelixBanan/Pascal/Lib/RedLib
 *)
 
@@ -18,11 +18,11 @@ procedure wrc(text: object; color: integer := 15);
 ///Вместо запятой, ставится + для объединения переменных
 ///Пример: wrcl(a+b+'test'+1, 10);
 procedure wrcl(text: object; color: integer := 15);
-///Это тот же write только укороченный
+///Укороченный write
 ///Вместо запятой, ставится + для объединения переменных
 ///Пример: wr(a+b+'test'+1);
 procedure wr(text: object);
-///Это тот же writeln только укороченный
+///Укороченный writeln
 ///Вместо запятой, ставится + для объединения переменных
 ///Пример: wrl(a+b+'test'+1);
 procedure wrl(text: object);
@@ -39,11 +39,116 @@ procedure BackCol(c: integer);
 ///Задержка
 procedure dly(ms: integer);
 
+/// Укороченный read
 procedure rd(var x: integer);
+procedure rd(var x: shortint);
+procedure rd(var x: smallint);
+procedure rd(var x: int64);
+procedure rd(var x: byte);
+procedure rd(var x: word);
+procedure rd(var x: longword);
+procedure rd(var x: uint64);
+procedure rd(var x: real);
+procedure rd(var x: single);
+
+/// Укороченный readln
+procedure rdl(var x: integer);
+procedure rdl(var x: shortint);
+procedure rdl(var x: smallint);
+procedure rdl(var x: int64);
+procedure rdl(var x: byte);
+procedure rdl(var x: word);
+procedure rdl(var x: longword);
+procedure rdl(var x: uint64);
+procedure rdl(var x: real);
+procedure rdl(var x: single);
 
 implementation
 
 procedure rd(var x: integer);
+begin
+  read(x);
+end;
+
+procedure rd(var x: shortint);
+begin
+  read(x);
+end;
+
+procedure rd(var x: smallint);
+begin
+  read(x);
+end;
+
+procedure rd(var x: int64);
+begin
+  read(x);
+end;
+procedure rd(var x: byte);
+begin
+  read(x);
+end;
+procedure rd(var x: word);
+begin
+  read(x);
+end;
+procedure rd(var x: longword);
+begin
+  read(x);
+end;
+procedure rd(var x: uint64);
+begin
+  read(x);
+end;
+procedure rd(var x: real);
+begin
+  read(x);
+end;
+procedure rd(var x: single);
+begin
+  read(x);
+end;
+
+procedure rdl(var x: integer);
+begin
+  read(x);
+end;
+
+procedure rdl(var x: shortint);
+begin
+  read(x);
+end;
+
+procedure rdl(var x: smallint);
+begin
+  read(x);
+end;
+
+procedure rdl(var x: int64);
+begin
+  read(x);
+end;
+procedure rdl(var x: byte);
+begin
+  read(x);
+end;
+procedure rdl(var x: word);
+begin
+  read(x);
+end;
+procedure rdl(var x: longword);
+begin
+  read(x);
+end;
+procedure rdl(var x: uint64);
+begin
+  read(x);
+end;
+procedure rdl(var x: real);
+begin
+  read(x);
+end;
+procedure rdl(var x: single);
 begin
   read(x);
 end;
@@ -110,7 +215,7 @@ var
 
 begin
   wrcl('*************************************', 12);
-  wrcl('Red Library version 1.1.0 (test branch)', 12);
+  wrcl('Red Library version 1.3.0 (test branch)', 12);
   wrcl('Lib for simplify coding', 12);
   wrcl('by FelixBanan', 12);
   wrcl('*************************************', 12);
